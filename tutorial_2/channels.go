1d1dcebf5eb948457a648f90daad38ff9d95fa4c
khushidesai23 <kmdesai23@gmail.com>
2026-05-04 17:17:45 +0530

    fix: adjust channel buffer size and processing loop for correct output

--- tutorial_2/channels.go
+++ tutorial_2/channels.go
@@ -1,19 +1,23 @@
 package main
-import ("fmt"
+import (
+	"fmt"
+	"time"
 )
 
 func main(){
-	var c = make (chan int)
+	var c = make (chan int, 5)
 	go process(c)
 	for i:= range c{
 		fmt.Println(i)
+		time.Sleep(time.Second*1)
 	}
 }
 
 func process(c chan int){
 	defer close(c)
-	for i:=0; i<10; i++{
+	for i:=0; i<5; i++{
 		c <- i
 	}
+	fmt.Println("Done processing")
 //	close(c)
 }
