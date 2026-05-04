0b723f81f55a2ecdada99b6b35d58c0c05eb7eac
khushidesai23 <kmdesai23@gmail.com>
2026-05-04 16:46:18 +0530

    add function to demonstrate pointers and array manipulation in Go

--- tutorial_1/pr7.go
+++ tutorial_1/pr7.go
@@ -29,5 +29,21 @@ func main(){
 	fmt.Printf("Original slice: %v\n", slice)
 	fmt.Printf("Copied slice: %v\n", sliceCopy)
 
+	fmt.Println("** Pointers to a function **")
+
+	var thing1 = [5]float64{1,2,3,4,5}
+	fmt.Printf("\n The memory location of the thing1 array is %p\n", &thing1)
+
+	var result [5]float64 = square(&thing1)
+	fmt.Printf("\n The squared values are %v\n", result)
+	fmt.Printf("\n The value of the thing1 array is %v\n", thing1)
+}
+
+func square(thing2 *[5]float64) [5]float64{
+	var thing [5]float64
+	for i:=0; i<5; i++{
+		thing[i] = (*thing2)[i]*(*thing2)[i]
+	}
+	return thing
 }
 
