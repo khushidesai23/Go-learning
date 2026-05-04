83345cc39c3a82c9532721fced3c9fb12ef71816
khushidesai23 <kmdesai23@gmail.com>
2026-05-04 17:16:02 +0530

    add: implement basic channel example for concurrent processing in Go

---
+++ tutorial_2/channels.go
@@ -0,0 +1,19 @@
+package main
+import ("fmt"
+)
+
+func main(){
+	var c = make (chan int)
+	go process(c)
+	for i:= range c{
+		fmt.Println(i)
+	}
+}
+
+func process(c chan int){
+	defer close(c)
+	for i:=0; i<10; i++{
+		c <- i
+	}
+//	close(c)
+}
