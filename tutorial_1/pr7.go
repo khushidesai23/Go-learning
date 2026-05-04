f181c190f8a27fba69b0264640182e5ace42930d
khushidesai23 <kmdesai23@gmail.com>
2026-05-04 16:38:33 +0530

    add slice manipulation demonstration to pointers tutorial

--- tutorial_1/pr7.go
+++ tutorial_1/pr7.go
@@ -20,5 +20,14 @@ func main(){
 	*p1 = 30
 	fmt.Printf("Value at address p1 after change: %v\n", *p1)
 	fmt.Printf("Value of i is: %v\n", i)
+
+	fmt.Println("** Pointer to a slice **")
+
+	var slice = []int32{1,2,3}
+	var sliceCopy = slice
+	sliceCopy[2] = 4
+	fmt.Printf("Original slice: %v\n", slice)
+	fmt.Printf("Copied slice: %v\n", sliceCopy)
+
 }
 
