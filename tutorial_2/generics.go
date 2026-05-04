71279371de9721867d5ee8357acab38725cb382d
khushidesai23 <kmdesai23@gmail.com>
2026-05-04 17:38:04 +0530

    refactor: replace isEmpty function with sumSlice for slice summation

--- tutorial_2/generics.go
+++ tutorial_2/generics.go
@@ -2,14 +2,33 @@ package main
 import "fmt"
 
 func main(){
-	var intSlice = []int{}
-	fmt.Println(isEmpty[int](intSlice))
+	var intSlice []int = []int{1, 2, 3, 4, 5}
+	fmt.Println(sumSlice[int](intSlice))
 
 	var float32Slice = []float32{1.1, 2.2, 3.3, 4.4, 5.5}
-	fmt.Println(isEmpty[float32](float32Slice))
+	fmt.Println(sumSlice[float32](float32Slice))
 }
 
-func isEmpty[T any](slice []T) bool {
-	return len(slice) == 0
+func sumSlice[T int | float32 | float64](slice []T) T{
+	var sum T
+	for _, v := range slice{
+		sum += v
+	}
+	return sum
 }
 
+
+// package main
+// import "fmt"
+
+// func main(){
+// 	var intSlice = []int{}
+// 	fmt.Println(isEmpty[int](intSlice))
+
+// 	var float32Slice = []float32{1.1, 2.2, 3.3, 4.4, 5.5}
+// 	fmt.Println(isEmpty[float32](float32Slice))
+// }
+
+// func isEmpty[T any](slice []T) bool {
+// 	return len(slice) == 0
+// }
