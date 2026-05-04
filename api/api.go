bef02603959ff3461f522a061a6c668755a57c79
khushidesai23 <kmdesai23@gmail.com>
2026-05-04 18:00:36 +0530

    add: implement API structures for coin balance parameters and responses

---
+++ api/api.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Coint Balance Params
+type CoinBalanceParams struct {
+	Username string
+}
+
+// Coin Balance Response
+type CoinBalanceResponse struct {
+	// Success Code, Usually 200
+	Code int
+
+	// Account Balance
+	Balance int64
+}
+
+// Error Response
+type Error struct {
+	// Error code
+	Code int
+
+	// Error message
+	Message string
+}
