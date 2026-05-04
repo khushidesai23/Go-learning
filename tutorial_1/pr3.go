86a44efd80e789dc6d929bfd82e066702befe593
khushidesai23 <kmdesai23@gmail.com>
2026-05-04 15:26:23 +0530

    add pr3.go file with example code demonstrating error handling in integer division

---
+++ tutorial_1/pr3.go
@@ -0,0 +1,33 @@
+package main
+import "fmt"
+import "errors"
+
+func main(){
+	var printValue string = "Hello, World! Khushi here"
+	printMe(printValue)
+
+	numerator := 11
+	denominator := 0
+	result, remainder, err := intDivision(numerator, denominator)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Printf("The result of %d divided by %d is %d with a remainder of %d\n", numerator, denominator, result, remainder)
+	}
+}
+
+func printMe(printValue string){
+	fmt.Println(printValue)
+}
+
+func intDivision(numerator int, denominator int) (int, int, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("denominator cannot be zero")
+		return 0, 0, err
+	}
+	
+	var result int = numerator / denominator
+	var remainder int = numerator % denominator
+	return result, remainder, err
+}
