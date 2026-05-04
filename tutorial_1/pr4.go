d6262a834798f00764de17555b1050672a8b6ad4
khushidesai23 <kmdesai23@gmail.com>
2026-05-04 15:46:53 +0530

    simplify pr4.go by removing unused array initialization and memory address printing

--- tutorial_1/pr4.go
+++ tutorial_1/pr4.go
@@ -2,15 +2,6 @@ package main
 import "fmt"
 
 func main(){
-	var intArr [5]int32
-	intArr[1] = 10
-	fmt.Println(intArr[0])
-	fmt.Println(intArr[1:3])
-
-	fmt.Println(len(intArr))
-	fmt.Println(cap(intArr))
-
-	fmt.Println(&intArr[0])
-	fmt.Println(&intArr[1])
-	fmt.Println(&intArr[2])
+	intArr := [...]int32{1, 2, 3}
+	fmt.Println(intArr)
 }
