656c19eddbfcb191ce53c5f41ea7845d47708c7c
khushidesai23 <kmdesai23@gmail.com>
2026-05-04 15:53:14 +0530

    add example code for slice length and capacity demonstration in pr4.go

--- tutorial_1/pr4.go
+++ tutorial_1/pr4.go
@@ -4,4 +4,16 @@ import "fmt"
 func main(){
 	intArr := [...]int32{1, 2, 3}
 	fmt.Println(intArr)
+
+	var intSlice []int32 = []int32{4, 5, 6}
+	fmt.Printf("The length of the slice is %v and the capacity is %v\n", len(intSlice), cap(intSlice))
+	intSlice = append(intSlice, 7)
+	fmt.Printf("The length of the slice is %v and the capacity is %v\n", len(intSlice), cap(intSlice))
+
+	var intSlice2 []int32 = []int32{8,9}
+	intSlice = append(intSlice, intSlice2...)
+	fmt.Println(intSlice)
+
+	var intSlice3 []int32 = make([]int32, 3, 8)
+	fmt.Println(intSlice3)
 }
