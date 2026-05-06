612df7611f5da0a8be85dbbaffe5f99cfdf5a6fd
khushidesai23 <kmdesai23@gmail.com>
2026-05-06 10:04:13 +0530

    fix: update import path for handlers in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/avukadin/goapi/internal/handlers"
+	"github.com/khushidesai23/Go-learning/tree/feature/building_api/internal/handlers"
 	log "github.com/sirupsen/logrus"
 )
 
